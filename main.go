00be38741255018c301acdf2a156e53b0acfb034
若海 <wang@rehiy.com>
2025-08-12 13:50:24 +0000

    fix: Correct file system variable usage in main function

--- main.go
+++ main.go
@@ -10,18 +10,15 @@ import (
 )
 
 //go:embed public/*
-var efs embed.FS
+var publicFS embed.FS
 
 func main() {
 	server.InitConfig()
 	server.InitRouter()
 
 	// 根路径直接绑定到 /public 目录
-	publicFS, err := fs.Sub(efs, "public")
-	if err != nil {
-		log.Fatal("Failed to create sub filesystem:", err)
-	}
-	http.Handle("/", http.FileServer(http.FS(publicFS)))
+	efs, _ := fs.Sub(publicFS, "public")
+	http.Handle("/", http.FileServer(http.FS(efs)))
 
 	log.Println("Server started at :8080")
 	http.ListenAndServe("0.0.0.0:8080", nil)
