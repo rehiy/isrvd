69f9d7b746fb795046e4842ee2236cd5ee71175e
若海 <wang@rehiy.com>
2025-08-13 14:39:57 +0000

    feat: 更新模态框组件，增加自定义样式和功能

--- public/components/modal-base.js
+++ public/components/modal-base.js
@@ -10,18 +10,21 @@ export const BaseModal = defineComponent({
         title: { type: String, required: true },
         size: { type: String, default: '' }, // '' | 'modal-lg' | 'modal-xl'
         loading: { type: Boolean, default: false },
-        confirmDisabled: { type: Boolean, default: false }
+        centered: { type: Boolean, default: true },
+        headerClass: { type: String, default: '' },
+        bodyClass: { type: String, default: '' },
+        showFooter: { type: Boolean, default: true },
+        confirmDisabled: { type: Boolean, default: false },
     },
     emits: ['confirm', 'cancel', 'shown', 'hidden'],
     setup(props, { emit, expose }) {
         let modalInstance = null;
 
         const show = () => {
-            const modalEl = document.getElementById(props.id);
-            if (modalEl) {
-                modalInstance = new bootstrap.Modal(modalEl);
+            const el = document.getElementById(props.id);
+            if (el) {
+                modalInstance = new bootstrap.Modal(el);
                 modalInstance.show();
-                emit('shown');
             }
         };
 
@@ -41,9 +44,12 @@ export const BaseModal = defineComponent({
         };
 
         onMounted(() => {
-            const modalEl = document.getElementById(props.id);
-            if (modalEl) {
-                modalEl.addEventListener('hidden.bs.modal', () => {
+            const el = document.getElementById(props.id);
+            if (el) {
+                el.addEventListener('shown.bs.modal', () => {
+                    emit('shown');
+                });
+                el.addEventListener('hidden.bs.modal', () => {
                     emit('hidden');
                 });
             }
@@ -58,23 +64,27 @@ export const BaseModal = defineComponent({
     },
     template: `
         <div class="modal fade" :id="id" tabindex="-1">
-            <div class="modal-dialog" :class="size">
+            <div class="modal-dialog" :class="[size, { 'modal-dialog-centered': centered }]">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">{{ title }}</h5>
+                    <div class="modal-header" :class="headerClass">
+                        <h5 class="modal-title">
+                            <slot name="title">{{ title }}</slot>
+                        </h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" :disabled="loading"></button>
                     </div>
-                    <div class="modal-body">
+                    <div class="modal-body" :class="bodyClass">
                         <slot></slot>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" @click="handleCancel" :disabled="loading">
-                            <slot name="cancel-text">取消</slot>
-                        </button>
-                        <button type="button" class="btn btn-primary" @click="handleConfirm" :disabled="loading || confirmDisabled">
-                            <i class="fas fa-spinner fa-spin" v-if="loading"></i>
-                            <slot name="confirm-text">确认</slot>
-                        </button>
+                    <div class="modal-footer" v-if="showFooter">
+                        <slot name="footer">
+                            <button type="button" class="btn btn-secondary" @click="handleCancel" :disabled="loading">
+                                <slot name="cancel-text">取消</slot>
+                            </button>
+                            <button type="button" class="btn btn-primary" @click="handleConfirm" :disabled="loading || confirmDisabled">
+                                <i class="fas fa-spinner fa-spin" v-if="loading"></i>
+                                <slot name="confirm-text">确认</slot>
+                            </button>
+                        </slot>
                     </div>
                 </div>
             </div>
--- public/components/modals/shell-modal.js
+++ public/components/modals/shell-modal.js
@@ -3,57 +3,60 @@
 const { defineComponent, ref } = Vue;
 
 import * as ShellTerminal from '../../helpers/shell.js';
+import { BaseModal } from '../modal-base.js';
 
 export const ShellModal = defineComponent({
     name: 'ShellModal',
+    components: { BaseModal },
     setup() {
         const modalRef = ref(null);
 
         const show = () => {
-            const modalEl = document.getElementById('shellModal');
-            if (modalEl) {
-                const modal = new bootstrap.Modal(modalEl);
-                modal.show();
+            modalRef.value.show();
+        };
 
-                // 延迟挂载，确保 DOM 已渲染
-                setTimeout(() => {
-                    const mountPoint = document.getElementById('xterm-container');
-                    if (mountPoint) {
-                        ShellTerminal.create(mountPoint);
-                    }
-                }, 200);
+        const hide = () => {
+            modalRef.value.hide();
+        };
 
-                // 监听弹窗关闭，自动卸载 terminal
-                if (!modalEl.__shellModalListener) {
-                    modalEl.addEventListener('hidden.bs.modal', () => {
-                        ShellTerminal.destroy();
-                        const mountPoint = document.getElementById('xterm-container');
-                        if (mountPoint) {
-                            mountPoint.innerHTML = '';
-                        }
-                    });
-                    modalEl.__shellModalListener = true;
-                }
+        const handleShown = () => {
+            const mountPoint = document.getElementById('xterm-container');
+            if (mountPoint) {
+                ShellTerminal.create(mountPoint);
             }
         };
 
-        return { show };
+        const handleHidden = () => {
+            ShellTerminal.destroy();
+            const mountPoint = document.getElementById('xterm-container');
+            if (mountPoint) {
+                mountPoint.innerHTML = '';
+            }
+        };
+
+        return {
+            show,
+            hide,
+            modalRef,
+            handleShown,
+            handleHidden
+        };
     },
     template: `
-        <div class="modal fade" id="shellModal" tabindex="-1" aria-labelledby="shellModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-xl modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header border-0">
-                        <h5 class="modal-title" id="shellModalLabel">
-                            <i class="fas fa-terminal"></i> 实时 Shell 终端
-                        </h5>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"></button>
-                    </div>
-                    <div class="modal-body p-0">
-                        <div id="xterm-container" style="height:400px;width:100%;background:#222;"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <BaseModal
+            ref="modalRef"
+            id="shellModal"
+            title="实时 Shell 终端"
+            size="modal-xl"
+            :show-footer="false"
+            body-class="bg-dark pe-0"
+            @shown="handleShown"
+            @hidden="handleHidden"
+        >
+            <template #title>
+                <i class="fas fa-terminal"></i> 实时 Shell 终端
+            </template>
+            <div id="xterm-container"></div>
+        </BaseModal>
     `
 });
