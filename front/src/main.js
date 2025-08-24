b1916af43772b2cc4f35d77d4154b0fb860c0862
若海 <wang@rehiy.com>
2025-08-24 14:49:29 +0000

    feat: 优化样式导入顺序，移除不必要的注释

--- front/src/main.js
+++ front/src/main.js
@@ -2,15 +2,15 @@ import { createApp } from 'vue'
 import App from '@/app.vue'
 
 // 导入全局样式
+import * as bootstrap from 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import '@xterm/xterm/css/xterm.css'
+
+// 导入自定义样式
 import './assets/style.css'
 
-// 导入并初始化 Bootstrap
-import * as bootstrap from 'bootstrap'
-
-// 确保 bootstrap 对象可在全局访问
+// 暴露 bootstrap
 window.bootstrap = bootstrap
 
 // 创建并挂载应用
